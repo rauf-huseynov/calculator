0c16f449fb38e7257a812ce698a06f801585a9cd
rfhusv <rfhusv@gmail.com>
2023-02-20 13:04:03 +0400

    Create basic operations (add, subtract, multiply, divide). Crate working display

--- javascript.js
+++ javascript.js
@@ -37,3 +37,61 @@ function squareRoot(a) {
         return Math.sqrt(a);
     }
 }
+
+const numberButtons = document.querySelectorAll('.numbers');
+numberButtons.forEach(numberBtn => {
+    numberBtn.addEventListener('click', () => {
+        value = parseInt(numberBtn.value);
+        const main = document.querySelector('#main');
+        if(main.textContent == 0) {
+            main.textContent = value;
+        } else {
+            if(main.textContent.length < 15) {
+                main.textContent+=value;
+            }
+        }
+    })
+})
+
+const twoOperatorButtons = document.querySelectorAll('.twoOperators');
+twoOperatorButtons.forEach(operatorBtn => {
+    operatorBtn.addEventListener('click', () => {
+        const main = document.querySelector('#main');
+        const value = operatorBtn.getAttribute('data-value');
+        const secondary = document.querySelector('#secondary');
+        if (secondary.textContent.length>0) {
+            const a = parseInt(secondary.textContent);
+            const b = parseInt(main.textContent);
+            const operatorId = operatorBtn.getAttribute('id');
+            switch(operatorId) {
+                case "percent":
+                    result = percent(a, b);
+                    break;
+                case "divide":
+                    result = divide(a, b);
+                    break;
+                case "multiply":
+                    result = multiply(a, b);
+                    break;
+                case "subtract":
+                    result = subtract(a, b);
+                    break;
+                case "add":
+                    result = add(a, b);
+                    break;
+            }
+            if (typeof result === 'number') {
+                secondary.textContent = result + value;
+                main.textContent = 0;
+            } else {
+                secondary.textContent = "";
+                main.textContent = result;
+            }
+        } else {
+            if(main.textContent.length>0) {
+                secondary.textContent = main.textContent + value;
+                main.textContent = 0;
+            }
+        }
+    })
+})
